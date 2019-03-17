b1bb5a19052105fe5200047014e150ee574c4c04
Omari <omari.bennett@gmail.com>
2019-03-16 22:35:31 -0400

    Add basic layout to the moblie & tablet homepage.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 'use strict'
 
 /* File imports */
-import './img/hexagon.jpg';
 import './sass/application.scss';
+
+/* Image File imports */
+import './img/hexagon.jpg';
+import './img/axel-antas-bergkvist-18903-unsplash.jpg';
 
 console.log(`This is a test of  the app set up.`)
