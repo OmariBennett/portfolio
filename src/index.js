25cb878eadc72ebc514df0f9497c5c65a6697250
Omari Bennett <omari.bennett@gmail.com>
2019-03-28 22:39:07 -0400

    finish the welcome page styling layout

--- src/index.js
+++ src/index.js
@@ -6,5 +6,6 @@ import './sass/application.scss';
 /* Image File imports */
 import './img/hexagon.jpg';
 import './img/axel-antas-bergkvist-18903-unsplash.jpg';
+import './img/white_wall.png';
 
 console.log(`This is a test of  the app set up.`)
